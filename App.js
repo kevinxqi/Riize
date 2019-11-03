bb000eff8f454fd47b1d2bb7409d0d1ec5e89a78
kevinxqi <kevinqi12@gmail.com>
2019-11-02 23:45:47 -0500

    Emotions Screen Finished

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import * as React from 'react';
 import { Button, View, Text, StyleSheet} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { Ionicons } from '@expo/vector-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faSmileBeam } from '@fortawesome/free-solid-svg-icons'
+import { faFrown } from '@fortawesome/free-solid-svg-icons'
+import { faAngry } from '@fortawesome/free-solid-svg-icons'
+import { faGrimace } from '@fortawesome/free-solid-svg-icons'
+
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#03DAC6' }}>
-        <Text style={{fontSize: 20}}>Rise</Text>
+        <Text style={{fontSize: 50}}>Riize</Text>
         <Button style={{borderRadius: 40, backgroundColor: '#ffae42'}}
           title="Login"
           onPress={() => this.props.navigation.navigate('Emotions')}
@@ -21,13 +26,32 @@ class HomeScreen extends React.Component {
 class EmotionPickerScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>How Are You Feeling?</Text>
-        <Ionicons name="md-checkmark-circle" size={32} color="green" onPress = {() => console.log("pressed")}/>
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
+      <View style={{flex: 1, backgroundColor: '#03DAC6', justifyContent: "center"}}>
+
+        <Text style = {styles.text} >How Are You Feeling?</Text>
+        <View style = {styles.emoticons}>
+          <View>
+            <FontAwesomeIcon icon = {faSmileBeam} color = "yellow" size = {50} />
+            <Text style = { {textAlign: 'center', marginTop: 5}}>Happy</Text> 
+          </View>
+
+          <View>
+            <FontAwesomeIcon icon = {faFrown} color = "yellow" size = {50} />
+            <Text style = { {textAlign: 'center', marginTop: 5}}>Sad</Text>
+          </View>
+
+          <View>
+            <FontAwesomeIcon icon = {faAngry} color = "yellow" size = {50}/>
+            <Text style = { {textAlign: 'center', marginTop: 5} }>Mad</Text>
+          </View>
+           
+          <View >
+            <FontAwesomeIcon icon = {faGrimace} color = "yellow" size = {50}/>
+            <Text style = { {textAlign: 'center', marginTop: 5}}>Scared</Text>
+          </View>
+         
+        </View>
+       
       </View>
     );
   }
@@ -53,11 +77,26 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        borderRadius: 4,
-        borderWidth: 0.5,
-        borderColor: '#d6d7da',
+    emoticons: {
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      marginTop: 30
     },
+
+    text:{
+      fontSize: 30,
+      fontFamily: "Futura",
+      fontWeight: 'bold',
+      textAlign: 'center',
+      marginBottom: 30
+    },
+
+    container: {
+        alignContent: "center",
+        flexDirection: "row"
+    },
+
     title: {
         fontSize: 19,
         fontWeight: 'bold',
