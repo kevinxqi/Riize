de4e55d29f594a093630ac88d630678fcefead13
kevinxqi <kevinqi12@gmail.com>
2019-11-03 00:30:31 -0500

    Background image and white text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text, StyleSheet} from 'react-native';
+import { Button, View, Text, StyleSheet, ImageBackground } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
@@ -12,13 +12,17 @@ import { faGrimace } from '@fortawesome/free-solid-svg-icons'
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#03DAC6' }}>
-        <Text style={{fontSize: 50}}>Riize</Text>
-        <Button style={{borderRadius: 40, backgroundColor: '#ffae42'}}
-          title="Login"
-          onPress={() => this.props.navigation.navigate('Emotions')}
-        />
-      </View>
+      <ImageBackground source={require('./ScreenImages/LoginScreen.jpg')} style={styles.container}>
+
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 50, color: 'white' }}>Riize</Text>
+          <Button style={{ borderRadius: 40, backgroundColor: '#ffae42' }}
+            title="Login"
+            onPress={() => this.props.navigation.navigate('Emotions')}
+          />
+        </View>
+      </ImageBackground >
+     
     );
   }
 }
@@ -26,32 +30,32 @@ class HomeScreen extends React.Component {
 class EmotionPickerScreen extends React.Component {
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: '#03DAC6', justifyContent: "center"}}>
+      <View style={{ flex: 1, backgroundColor: '#03DAC6', justifyContent: "center" }}>
 
-        <Text style = {styles.text} >How Are You Feeling?</Text>
-        <View style = {styles.emoticons}>
+        <Text style={styles.text} >How Are You Feeling?</Text>
+        <View style={styles.emoticons}>
           <View>
-            <FontAwesomeIcon icon = {faSmileBeam} color = "yellow" size = {50} />
-            <Text style = { {textAlign: 'center', marginTop: 5}}>Happy</Text> 
+            <FontAwesomeIcon icon={faSmileBeam} color="yellow" size={50} />
+            <Text style={{ textAlign: 'center', marginTop: 5, color:'white' }}>Happy</Text>
           </View>
 
           <View>
-            <FontAwesomeIcon icon = {faFrown} color = "yellow" size = {50} />
-            <Text style = { {textAlign: 'center', marginTop: 5}}>Sad</Text>
+            <FontAwesomeIcon icon={faFrown} color="yellow" size={50} />
+            <Text style={{ textAlign: 'center', marginTop: 5, color: 'white' }}>Sad</Text>
           </View>
 
           <View>
-            <FontAwesomeIcon icon = {faAngry} color = "yellow" size = {50}/>
-            <Text style = { {textAlign: 'center', marginTop: 5} }>Mad</Text>
+            <FontAwesomeIcon icon={faAngry} color="yellow" size={50} />
+            <Text style={{ textAlign: 'center', marginTop: 5, color: 'white' }}>Mad</Text>
           </View>
-           
+
           <View >
-            <FontAwesomeIcon icon = {faGrimace} color = "yellow" size = {50}/>
-            <Text style = { {textAlign: 'center', marginTop: 5}}>Scared</Text>
+            <FontAwesomeIcon icon={faGrimace} color="yellow" size={50} />
+            <Text style={{ textAlign: 'center', marginTop: 5, color: 'white' }}>Scared</Text>
           </View>
-         
+
         </View>
-       
+
       </View>
     );
   }
@@ -77,31 +81,33 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    emoticons: {
-      flexDirection: "row",
-      justifyContent: "space-evenly",
-      alignItems: "center",
-      marginTop: 30
-    },
+  emoticons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginTop: 30
+  },
 
-    text:{
-      fontSize: 30,
-      fontFamily: "Futura",
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginBottom: 30
-    },
+  text: {
+    fontSize: 30,
+    color: 'white',
+    fontFamily: "Futura",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 30
+  },
 
-    container: {
-        alignContent: "center",
-        flexDirection: "row"
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: 'center'
+  },
 
-    title: {
-        fontSize: 19,
-        fontWeight: 'bold',
-    },
-    activeTitle: {
-        color: 'red',
-    },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  activeTitle: {
+    color: 'red',
+  },
 });
